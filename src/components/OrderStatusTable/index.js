7e5cfb95d66c8d00af5cb2df2afde6f374d05729
Abhishek <abhishekktech2@gmail.com>
2023-11-23 15:03:48 +0000

    Header Finished

---
+++ src/components/OrderStatusTable/index.js
@@ -0,0 +1,9 @@
+const OrderStatusTable = () => {
+  return (
+    <section>
+      <h1>Abhishek</h1>
+      <h1>Abhishek</h1>
+    </section>
+  );
+};
+export default OrderStatusTable;
--- src/components/Reeco/index.js
+++ src/components/Reeco/index.js
@@ -1,11 +1,13 @@
 import Navbar from "../navbar";
 import Header from "../OrderHeader";
+import OrderStatusTable from "../OrderStatusTable";
 import { AppContainer } from "./style";
 
 const Reeco = () => (
   <AppContainer>
     <Navbar />
     <Header />
+    <OrderStatusTable />
   </AppContainer>
 );
 export default Reeco;
